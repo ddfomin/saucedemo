47004113bf8acca3d02337a37da24d7d805f9266
ddfomin <dmdmfomin@gmail.com>
2026-09-17 17:20:50 +0500

    edit conftest, add .github

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,8 +16,14 @@ def driver():
         "autofill.credit_card_enabled": False
     }
     options.add_experimental_option("prefs", prefs)
+
     # Дополнительные аргументы
-    # options.add_argument("--headless")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    # Дополнительные аргументы
+
     options.add_argument("--disable-save-password-bubble")
     options.add_argument("--disable-password-generation")
     driver = webdriver.Chrome(options=options)
